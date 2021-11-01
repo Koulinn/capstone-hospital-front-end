1370470c887c38e35aa575400fc10a42b09514bd
Koulinn <drdverzola@gmail.com>
2021-11-01 08:36:28 +0000

    npm packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Chat from './views/Chat/Chat';
+import Login from './views/Login/Login'
 
 function App() {
+
   return (
-    <div className="App">
-     <h1>REACT</h1>
-    </div>
+  
+        <Router>
+          <Route path="/" exact render={(routerProps) =>
+            <Chat {...routerProps} />}>
+          </Route>
+        </Router>
   );
 }
 
